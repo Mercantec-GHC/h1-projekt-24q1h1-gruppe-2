9ba4ad56881e2e6ad98f5829dd109693def968bf
ansebanse <anni0007@edu.mercantec.dk>
2024-03-13 19:11:02 +0100

    update

---
+++ BlazorApp/Components/Pages/Sound.cs
@@ -0,0 +1,18 @@
+﻿namespace Sound
+{
+    public class Sound
+    {
+        public string Condition { get; set; }
+        public int Price { get; set; }
+        public string Genre { get; set; }
+        public string Brand { get; set; }
+        public string Album { get; set; }
+        public string Artist { get; set; }
+
+    }
+
+    public class Kassettebånd
+    {
+
+    }
+}
