bf427cae01a8eb60d7a20e6ecc71ddfafff865d8
ansebanse <anni0007@edu.mercantec.dk>
2024-03-26 12:28:47 +0100

    Update

--- Domain Models/Itemdata.cs
+++ Domain Models/Itemdata.cs
@@ -8,7 +8,7 @@ namespace Domain_Models
             List<Sound> AllItemdata = new List<Sound>();
             AllItemdata.Add(new Sound
             {
-                id = 1,
+                id = 0,
                 name = "My Man",
                 artist = "Ben Webster",
                 condition = "MINT",
@@ -24,7 +24,7 @@ namespace Domain_Models
             });
             AllItemdata.Add(new Sound
             {
-                id = 2,
+                id = 1,
                 name = "Take the 'A' Train",
                 artist = "Billy Strayhorn",
                 condition = "VG+",
@@ -38,6 +38,24 @@ namespace Domain_Models
                 tracks = "Take the 'A' Train, Cheek to Cheek, What Is This Thing Called Love?, I Can't Get Started, St. Louis Blues",
                 favorite = true,
             });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 2,
+                name = "Blood on the Tracks",
+                artist = "Bob Dylan",
+                condition = "Very Good",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1975,
+                price = 180,
+                genre = "Folk Rock",
+                album = "Blood on the Tracks",
+                brand = "Columbia Records",
+                tracks = "Tangled Up in Blue, Simple Twist of Fate, You're a Big Girl Now, Idiot Wind, You're Gonna Make Me Lonesome When You Go, Meet Me in the Morning, Lily, Rosemary and the Jack of Hearts, If You See Her, Say Hello, Shelter from the Storm, Buckets of Rain",
+                favorite = false,
+            });
+
 
             AllItemdata.Add(new Sound
             {
@@ -174,6 +192,228 @@ namespace Domain_Models
                 tracks = "Second Hand News, Dreams, Never Going",
                 favorite = false,
 
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 11,
+                name = "Back in Black",
+                artist = "AC/DC",
+                condition = "Good",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1980,
+                price = 100,
+                genre = "Hard Rock",
+                album = "Back in Black",
+                brand = "Atlantic Records",
+                tracks = "Hells Bells, Shoot to Thrill, What Do You Do for Money Honey, Given the Dog a Bone, Let Me Put My Love into You, Back in Black, You Shook Me All Night Long, Have a Drink on Me, Shake a Leg, Rock and Roll Ain't Noise Pollution",
+                favorite = false,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 12,
+                name = "The Wall",
+                artist = "Pink Floyd",
+                condition = "Very Good",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1979,
+                price = 180,
+                genre = "Progressive Rock",
+                album = "The Wall",
+                brand = "Harvest Records",
+                tracks = "In the Flesh?, The Thin Ice, Another Brick in the Wall Part 1, The Happiest Days of Our Lives, Another Brick in the Wall Part 2, Mother, Goodbye Blue Sky, Empty Spaces, Young Lust, One of My Turns, Don't Leave Me Now, Another Brick in the Wall Part 3, Goodbye Cruel World",
+                favorite = true,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 13,
+                name = "The Chronic",
+                artist = "Dr. Dre",
+                condition = "Near Mint",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1992,
+                price = 250,
+                genre = "Hip Hop, G-Funk",
+                album = "The Chronic",
+                brand = "Death Row Records",
+                tracks = "The Chronic (Intro), Fuck wit Dre Day (And Everybody's Celebratin'), Let Me Ride, The Day the Niggaz Took Over, Nuthin' but a 'G' Thang, Deeez Nuuuts, Lil' Ghetto Boy, A Nigga Witta Gun, Rat-Tat-Tat-Tat, The $20 Sack Pyramid, Lyrical Gangbang, High Powered, The Doctor's Office, Stranded on Death Row, The Roach (The Chronic Outro), Bitches Ain't Shit",
+                favorite = false,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 14,
+                name = "Nevermind",
+                artist = "Nirvana",
+                condition = "Excellent",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1991,
+                price = 180,
+                genre = "Grunge, Alternative Rock",
+                album = "Nevermind",
+                brand = "DGC Records",
+                tracks = "Smells Like Teen Spirit, In Bloom, Come as You Are, Breed, Lithium, Polly, Territorial Pissings, Drain You, Lounge Act, Stay Away, On a Plain, Something in the Way",
+                favorite = true,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 15,
+                name = "Purple Rain",
+                artist = "Prince and The Revolution",
+                condition = "Very Good",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1984,
+                price = 160,
+                genre = "Pop, Rock, Funk",
+                album = "Purple Rain",
+                brand = "Warner Bros. Records",
+                tracks = "Let's Go Crazy, Take Me with U, The Beautiful Ones, Computer Blue, Darling Nikki, When Doves Cry, I Would Die 4 U, Baby I'm a Star, Purple Rain",
+                favorite = false,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 16,
+                name = "Hotel California",
+                artist = "Eagles",
+                condition = "Good",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1976,
+                price = 120,
+                genre = "Rock, Soft Rock",
+                album = "Hotel California",
+                brand = "Asylum Records",
+                tracks = "Hotel California, New Kid in Town, Life in the Fast Lane, Wasted Time, Wasted Time (Reprise), Victim of Love, Pretty Maids All in a Row, Try and Love Again, The Last Resort",
+                favorite = true,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 17,
+                name = "Born to Run",
+                artist = "Bruce Springsteen",
+                condition = "Near Mint",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1975,
+                price = 180,
+                genre = "Rock",
+                album = "Born to Run",
+                brand = "Columbia Records",
+                tracks = "Thunder Road, Tenth Avenue Freeze-Out, Night, Backstreets, Born to Run, She's the One, Meeting Across the River, Jungleland",
+                favorite = false,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 18,
+                name = "Darkness on the Edge of Town",
+                artist = "Bruce Springsteen",
+                condition = "Excellent",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1978,
+                price = 200,
+                genre = "Rock",
+                album = "Darkness on the Edge of Town",
+                brand = "Columbia Records",
+                tracks = "Badlands, Adam Raised a Cain, Something in the Night, Candy's Room, Racing in the Street, The Promised Land",
+                favorite = false,
+
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 19,
+                name = "Thriller",
+                artist = "Michael Jackson",
+                condition = "Very Good",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1982,
+                price = 220,
+                genre = "Pop, R&B",
+                album = "Thriller",
+                brand = "Epic Records",
+                tracks = "Wanna Be Startin' Somethin', Baby Be Mine, The Girl Is Mine, Thriller, Beat It, Billie Jean, Human Nature, P.Y.T. (Pretty Young Thing), The Lady in My Life",
+                favorite = false,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 20,
+                name = "What's Going On",
+                artist = "Marvin Gaye",
+                condition = "Excellent",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1971,
+                price = 190,
+                genre = "Soul, R&B",
+                album = "What's Going On",
+                brand = "Tamla Records",
+                tracks = "What's Going On, What's Happening Brother, Flyin' High (In the Friendly Sky), Save the Children, God Is Love, Mercy Mercy Me (The Ecology), Right On, Wholy Holy, Inner City Blues (Make Me Wanna Holler)",
+                favorite = true,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 21,
+                name = "The Queen Is Dead",
+                artist = "The Smiths",
+                condition = "Good",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1986,
+                price = 150,
+                genre = "Alternative Rock, Indie Rock",
+                album = "The Queen Is Dead",
+                brand = "Rough Trade Records",
+                tracks = "The Queen Is Dead, Frankly, Mr. Shankly, I Know It's Over, Never Had No One Ever, Cemetry Gates, Bigmouth Strikes Again, The Boy with the Thorn in His Side, Vicar in a Tutu, There Is a Light That Never Goes Out, Some Girls Are Bigger Than Others",
+                favorite = false,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 22,
+                name = "Sgt. Pepper's Lonely Hearts Club Band",
+                artist = "The Beatles",
+                condition = "Near Mint",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1967,
+                price = 300,
+                genre = "Rock",
+                album = "Sgt. Pepper's Lonely Hearts Club Band",
+                brand = "Parlophone",
+                tracks = "Sgt. Pepper's Lonely Hearts Club Band, With a Little Help from My Friends, Lucy in the Sky with Diamonds, Getting Better, Fixing a Hole, She's Leaving Home, Being for the Benefit of Mr. Kite!, Within You Without You, When I'm Sixty-Four, Lovely Rita, Good Morning Good Morning, Sgt. Pepper's Lonely Hearts Club Band (Reprise), A Day in the Life",
+                favorite = true,
+            });
+
+            AllItemdata.Add(new Sound
+            {
+                id = 23,
+                name = "The Velvet Underground & Nico",
+                artist = "The Velvet Underground",
+                condition = "Good",
+                speed = "33 RPM",
+                size = "12''",
+                year = 1967,
+                price = 150,
+                genre = "Experimental Rock, Art Rock",
+                album = "The Velvet Underground & Nico",
+                brand = "Verve Records",
+                tracks = "Sunday Morning, I'm Waiting for the Man, Femme Fatale, Venus in Furs, Run Run Run, All Tomorrow's Parties, Heroin, There She Goes Again, I'll Be Your Mirror, The Black Angel's Death Song, European Son",
+                favorite = false,
             });
 
             return AllItemdata;
---
+++ Domain Models/Soundablegadget.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Models
+{
+    public abstract class Soundablegadget
+    {
+        public int gadgetId { get; set;}
+        public string url { get; set;}
+        public string name { get; set;}
+        public string brand {  get; set;}
+        public string color { get; set;}
+        public string type { get; set;}
+
+    }
+
+    public class Bombox : Soundablegadget
+    {
+        
+    }
+
+    public class Radio : Soundablegadget
+    {
+
+    }
+
+    public class Vinylplayer : Soundablegadget
+    {
+
+    }
+}
