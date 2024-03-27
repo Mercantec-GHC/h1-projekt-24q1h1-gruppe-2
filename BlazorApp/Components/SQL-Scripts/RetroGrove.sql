cc7db507389b74cb112ed7ec5b5d1fdbb7edf9f6
ansebanse <anni0007@edu.mercantec.dk>
2024-03-27 08:50:08 +0100

    updated

---
+++ BlazorApp/Components/SQL-Scripts/RetroGrove.sql
@@ -0,0 +1,15 @@
+CREATE TABLE sounds 
+(
+  id SERIAL  PRIMARY KEY,
+  Name TEXT,
+  Artist TEXT,
+  Condition TEXT, 
+  Speed TEXT,
+  Size TEXT,
+  Year INTEGER,
+  Price INTEGER,
+  Genre TEXT,
+  Album TEXT, 
+  Brand TEXT,
+  Tracks TEXT
+);
